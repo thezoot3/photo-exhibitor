05706cc351dbb2a3938ab92302e4253328cd8911
thezoot3 <61928790+thezoot3@users.noreply.github.com>
2022-08-20 00:28:35 +0900

    update photoAlbum.js

--- src/redux/photoAlbum.js
+++ src/redux/photoAlbum.js
@@ -1,11 +1,13 @@
 import {checkRequireArgs, checkTypeArgs, applyMultipleCheck} from '../utils/arg_check'
 const defaultAlbum = {
     query: {},
-    images: {}
+    images: {},
+    fetcher: () => {}
 }
 const SET_QUERY = "SET_QUERY";
 const SET_IMAGES = "SET_IMAGES";
 const CLEAR_IMAGES = "CLEAR_IMAGES";
+const SET_FETCHER = "SET_FETCHER"'
 export const setQuery = applyMultipleCheck((query = {}) => {
   return {
     type: SET_QUERY,
@@ -21,6 +23,12 @@ export const setImages = applyMultipleCheck((imageData) => {
 export const clearImages = () => {
   return {type: CLEAR_IMAGES}
 }
+export const setFetcher = applyMultipleCheck((fetcher) => {
+    return {
+        type: SET_FETCHER,
+        fetcher
+    }
+}, [checkRequireArgs, [1]], [checkTypeArgs, [Function]])
 export const reducer = (state = defaultAlbum, action) => {
   switch(action.type) {
     case SET_QUERY:
@@ -29,6 +37,8 @@ export const reducer = (state = defaultAlbum, action) => {
       return {...state, imageData: action.imageData}
     case CLEAR_IMAGES: 
       return {...state, imageData: {}}
+    case SET_FETCHER:
+        return {...state, fetcher: state.fetcher}
     default:
       return state
   }
