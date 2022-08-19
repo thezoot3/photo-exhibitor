d0b60576b23efe00e1135ac61da422d04cec896a
thezoot3 <61928790+thezoot3@users.noreply.github.com>
2022-08-19 23:30:34 +0900

    Implemention Complete

--- src/redux/photoAlbum.js
+++ src/redux/photoAlbum.js
@@ -1,6 +1,35 @@
+import {checkRequireArgs, checkTypeArgs, applyMultipleCheck} from '../utils/arg_check'
 const defaultAlbum = {
-    "1": 1
+    query: {},
+    images: {}
+}
+const SET_QUERY = "SET_QUERY";
+const SET_IMAGES = "SET_IMAGES";
+const CLEAR_IMAGES = "CLEAR_IMAGES";
+export const setQuery = applyMultipleCheck((query = {}) => {
+  return {
+    type: SET_QUERY,
+    query,
+  }
+}, [checkRequireArgs, [1]], [checkTypeArgs, [Object]])
+export const setImages = applyMultipleCheck((imageData) => {
+  return {
+    type: SET_IMAGES,
+    imageData,
+  }
+}, [checkRequireArgs, [1]], [checkTypeArgs, [Object]])
+export const clearImages = () => {
+  return {type: CLEAR_IMAGES}
 }
 export const reducer = (state = defaultAlbum, action) => {
-    return state
+  switch(action.type) {
+    case SET_QUERY:
+      return {...state, query: action.query}
+    case SET_IMAGES:
+      return {...state, imageData: action.imageData}
+    case CLEAR_IMAGES: 
+      return {...state, imageData: {}}
+    default:
+      return state
+  }
 }
