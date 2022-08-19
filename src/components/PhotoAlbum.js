4df0dba0120e1b1a3a34135356601850a42d38ed
thezoot3 <61928790+thezoot3@users.noreply.github.com>
2022-08-20 00:33:56 +0900

    Implemention Complete \

--- src/components/PhotoAlbum.js
+++ src/components/PhotoAlbum.js
@@ -1,9 +1,16 @@
 import {createStore} from "redux";
-import { reducer as photoReducer } from '../redux/photoAlbum';
+import { reducer as photoReducer, setFetcher } from '../redux/photoAlbum';
 import {Provider} from "react-redux";
 function PhotoAlbum({fetchData = () => {}, children, autoFetch = true}) {
     const store = createStore(photoReducer)
-    store.subscribe(state => {})
+    const fetcher = () => {
+        fetchData(store.getState().query)
+    }
+    store.subscribe(state => {
+        if(autoFetch) fetchData(state.query)
+    })
+    
+    store.dispatch(setFetcher(fetcher));
     return (
         <Provider store={store}>
             {children.constructor === Array ? children : [children]}
